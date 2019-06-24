17791dfd3e5728501586a68601fef76ffc7e8634
Shubham-Kant <pro.shubham1999@gmail.com>
2019-06-25 02:22:25 +0530

    added router for home

---
+++ controllers/home_controller.js
@@ -0,0 +1,4 @@
+//module.exports.action_name= function(req,res){}
+module.exports.home=function(req,res){
+   return  res.end('<h1>express is up for codeial</h1>');
+}
---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const router=express.Router();
+const homeController=require('../controllers/home_controller.js');
+
+router.get('/',homeController.home);
+
+
+console.log('router.express is working....');
+module.exports=router;
