3dce0d6510bf772a2ce3ef17b10050fc96c33444
Shubham-Kant <pro.shubham1999@gmail.com>
2019-07-01 01:59:53 +0530

    sign in is done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,4 +1,7 @@
 const User=require('../models/user');
+module.exports.profile=function(req,res){
+    return res.render('user_profile');
+}
 module.exports.signUp=function(req,res){
 
     return res.render('user_sign_up');
@@ -6,10 +9,10 @@ module.exports.signUp=function(req,res){
 }
 module.exports.signIn=function(req,res){
     //just printing the cookies on screen 
-    console.log(req.cookies);
+    // console.log(req.cookies);
     //setting up the cookies
-    res.cookie('user_id',25);
-    res.cookie('name','pheobe');
+    // res.cookie('user_id',25);
+    // res.cookie('name','pheobe');
     return res.render('user_sign_in');
 
 }
@@ -37,4 +40,39 @@ module.exports.create=function(req,res){
         }
         
     });
+}
+//sign in and create the session for the user
+module.exports.createSession=function(req,res){
+    //steps to authenticate
+    //find the user
+    User.findOne({email:req.body.email},function(err,user){
+        
+        if(err){
+            console.log('error finding user in signing in');
+            return;
+        }
+        // console.log(user);
+        //user mail is matched in the DB..
+        //a handle user is found
+        if(user){
+            //password doesnt match
+            if(user.password!=req.body.password){
+                return res.redirect('back');
+            }
+            //handle session created
+            res.cookie('user_id',user.id);
+            return res.redirect('/users/profile');
+
+
+            //password matches
+
+        }
+        //user mail is matched in the DB...
+        //handle user not found
+        else{
+             return res.redirect('back');
+        }
+
+    });
+
 }
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,9 @@ const usersController=require('../controllers/users_controller');
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
 router.use('/posts',require('./posts'));
-router.post('/create',usersController.create)
+router.post('/create',usersController.create);
+router.post('/create-session',usersController.createSession);
+router.get('/profile',usersController.profile);
 
 //ye file kahi par import karani padegi isliye yaha se export ki hai
 module.exports=router;
