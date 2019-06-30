8c8e3134933b3ab13c7e29a94d89f87c0954daff
Shubham-Kant <pro.shubham1999@gmail.com>
2019-06-30 23:51:46 +0530

    user sign up is done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,4 @@
+const User=require('../models/user');
 module.exports.signUp=function(req,res){
 
     return res.render('user_sign_up');
@@ -11,4 +12,29 @@ module.exports.signIn=function(req,res){
     res.cookie('name','pheobe');
     return res.render('user_sign_in');
 
+}
+//get the sign up data
+module.exports.create=function(req,res){
+    if(req.body.password!=req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email:req.body.email},function(err,user){
+        if(err){
+            console.log('error in finding user in signing up');
+            return;
+        }
+        if(!user){
+            User.create(req.body,function(err,user){
+                if(err){
+                    console.log('error in creating a user while signing up');
+                    return;
+                }
+                return res.redirect('/users/sign-in');
+            })
+        }
+        else{
+            return res.redirect('back');
+        }
+        
+    });
 }
--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ const usersController=require('../controllers/users_controller');
 router.get('/sign-up',usersController.signUp);
 router.get('/sign-in',usersController.signIn);
 router.use('/posts',require('./posts'));
+router.post('/create',usersController.create)
 
 //ye file kahi par import karani padegi isliye yaha se export ki hai
 module.exports=router;
