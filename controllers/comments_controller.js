021ab01efdde52d08e8a28990451be3fab3c77f9
Shubham-Kant <pro.shubham1999@gmail.com>
2019-07-11 21:24:19 +0530

    converted more code to Async Await

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,56 +1,53 @@
 const Comment=require('../models/comment');
 const Post=require('../models/post');
-
 //creating a comment
-module.exports.create=function(req,res){
-   Post.findById(req.body.post,function(err,post){
-    if(err){
-        console.log('error in fetching comments');
-        return ;
-    }
+module.exports.create=async function(req,res){
+  
+    try{
+       let post=await Post.findById(req.body.post);
        if(post){
            
-           Comment.create({
+           let comment=await Comment.create({
             //req.body contains content and post id
               content:req.body.content,
               post:req.body.post,
               user:req.user._id
-           },function(err,comment){
-               if(err){
-                   console.log('error in fetching comments');
-                   return ;
-               }
+           });
+               
                //pushing the comment in the field 'comments' of post
                //added comment to the post.
                post.comments.push(comment);
                //saving 
                post.save();
                return res.redirect('/');
-           });
        }
-   });
+    }
+    catch(err){
+        console.log('error in creting a comment',err);
+        return;
+    }
+   
 }
 //deleting a comment
-module.exports.destroy=function(req,res){
-    Comment.findById(req.params.id,function(err,comment){
-        
+module.exports.destroy=async function(req,res){
+    try{
+    let comment=await Comment.findById(req.params.id);
+
             let postId=comment.post;
-            Post.findById(postId,function(err,post){
+            let post=await Post.findById(postId);
                 let userId=post.user;
                 if(post.user==req.user.id||comment.user == req.user.id){
                     comment.remove(); 
                     let postId=comment.post;
-                    Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}},function(err,post){
-                   
-                        return res.redirect('back');
-                 });
+                    let post=await Post.findByIdAndUpdate(postId,{$pull:{comments:req.params.id}});
+
+                        return res.redirect('back');           
                 }
                 else{
                     return res.redirect('back');
                 }
-            });
-          
-        
-    });
-    
+    }
+    catch(err){
+        console.log('error in deleting a comment',err);
+    }       
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,36 +1,41 @@
 const Post=require('../models/post');
 const Comment=require('../models/comment');
-module.exports.create=function(req,res){
-   Post.create({
-       content:req.body.content,
-       //passing the user id.
-       user:req.user._id
-    },function(err){
-        if(err){
-            console.log('error in posting a post');
-            return;
-        }
+module.exports.create=async function(req,res){
+  
+    try{
+        await Post.create({
+           content:req.body.content,
+           //passing the user id.
+           user:req.user._id
+        });
         return res.redirect('back');
-    });
+    }
+    catch(err){
+        console.log('Error in creating a post',err);
+        return;
+    }
+     
 
 }
-module.exports.destroy=function(req,res){
+module.exports.destroy=async function(req,res){
     //req.params.id contains id of post to be deleted
-   Post.findById(req.params.id,function(err,post){
-       //post.user contains id of the user who posted that post.
-       //req.user contains info of current logged in user.
-       // .id means converting the object id into string.
-       if(post.user==req.user.id){
-           post.remove();
+    try{
+        let post=await Post.findById(req.params.id);
+            //post.user contains id of the user who posted that post.
+            //req.user contains info of current logged in user.
+            // .id means converting the object id into string.
+            if(post.user==req.user.id){
+                post.remove();
            
-           Comment.deleteMany({post:req.params.id},function(err){
-               return res.redirect('back');
-           });
-       }
-       else{
-           return res.redirect('back');
-       }
-    
-   });
-
+                await Comment.deleteMany({post:req.params.id});
+                return res.redirect('back');
+            }
+            else{
+                return res.redirect('back');
+            }
+        }
+    catch(err){
+        console.log('error in deleting a post',err);
+        return;
+    }
 }
