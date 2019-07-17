e0cdec2ac4b7c07ec15f94bdbedfd0f777f124e7
Shubham-Kant <pro.shubham1999@gmail.com>
2019-07-17 15:14:52 +0530

    Configuring Multer for user avatar:setting up the storage property

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const multer=require('multer');
 const path=require('path');
-const AVATAR_PATH=path.join('uploads/users/avatars');
+const AVATAR_PATH=path.join('/uploads/users/avatars');
 const userSchema=new mongoose.Schema({
     email:{
         type:String,
@@ -17,11 +17,27 @@ const userSchema=new mongoose.Schema({
     name:{
         type:String,
         required:true
+    },
+    avatar:{
+        type:String
     }
 
 },{
     //to add a feature of 'created at' and 'updated at'.
     timestamps:true
 });
+
+let storage=multer.diskStorage({
+    destination: function(req,file,cb){
+        cb(null,path.join(__dirname,'..',AVATAR_PATH));
+
+    },
+    filename: function(req,file,cb){
+        cb(null,file.fieldname + '-' + Date.now());
+    }
+})
+
+
+
 const User=mongoose.model('User',userSchema);
 module.exports=User;
