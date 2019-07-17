9a9e5c7b1542582d48c8aeea758c2fdea0986835
Shubham-Kant <pro.shubham1999@gmail.com>
2019-07-18 01:13:59 +0530

    Showing the avatar(here Profile Picture)

--- index.js
+++ index.js
@@ -38,6 +38,7 @@ app.use(sassMiddleware({
 app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(express.static('assets'));
+app.use('/uploads',express.static(__dirname+'/uploads'));
 app.use(expressLayouts);
 //extraxt style and scripts from subpages into the layout
 app.set('layout extractStyles',true);
