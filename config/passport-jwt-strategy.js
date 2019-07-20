8d1c47f1243ce5982a5cd453c3fe52f71feb5466
Shubham-Kant <pro.shubham1999@gmail.com>
2019-07-20 21:06:27 +0530

    Setting up Passport JWT

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,30 @@
+const passport=require('passport');
+const JWTStrategy=require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;
+
+const User=require('../models/user');
+let opts={
+    //finding jwt from request
+    jwtFromRequest:ExtractJWT.fromAuthHeaderAsBearerToken,
+    //key to encrypt/decrypt the token
+    secretOrKey:'codeial'
+}
+//telling passport to use jwt strategy
+passport.use(new JWTStrategy(opts,function(jwtPayLoad,done){
+   User.findById(jwtPayLoad._id,function(err,user){
+       if(err){
+           console.log('error in finding user from jwt');
+           return;
+       }
+       //if user is found
+       if(user){
+            return done(null,user);
+       }
+       //if user is not found
+       else{
+           return done(null,false);
+       }
+   })
+}));
+
+module.exports=passport;
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const db=require('./config/mongoose');
 const session=require('express-session');
 const passport=require('passport');
 const passportLocal=require('./config/passport-local-strategy');
+const passportJWT = require('./config/passport-jwt-strategy');
 //importing connect-mongo module and specifically a arguement named 'session'. 
 const MongoStore=require('connect-mongo')(session);
 
