72c83b531086d55ccac3130f10a7ad6f1db8cb20
Shubham-Kant <pro.shubham1999@gmail.com>
2019-07-30 19:10:20 +0530

    actions and routes::Likes

---
+++ controllers/likes_controller.js
@@ -0,0 +1,59 @@
+const Like=require('../models/like');
+const Post=require('../models/post');
+const Comment=require('../models/comment');
+
+module.exports.toggleLike=async function(req,res){
+    try{
+        let likeable;
+        let deleted=false;
+        // /likes/toggle/?id=abcde&type=Post
+        if(req.query.type=='Post'){
+            likeable=await Post.findById(req.body.id).populate('likes');
+        }
+        // /likes/toggle/?id=abcde&type=Comment
+        else{
+            likeable=await Comment.findById(req.body.id).populate('likes');
+        }
+        //check if a like already exists
+        let existingLike=await Like.findOne({
+            user:req.user._id,
+            likeable:req.query.id,
+            onModel:req.query.type
+
+        });
+        // if a like already exists then delete it
+        if(existingLike){
+            likeable.likes.pull(existingLike._id);
+            likeable.save();
+            existingLike.remove();
+            deleted=true;
+
+        }
+
+        else{
+            // else make a new like
+            let newLike=await Like.create({
+                user:req.user._id,
+                onModel:req.query.type,
+                likeable:req.query.id
+                
+            });
+            likeable.likes.push(newLike._id);
+            likeable.save();
+
+        }
+        return res.json(200,{
+            data:{
+                deleted:deleted
+            }
+        });
+
+    }
+    catch(err){
+        console.log(err);
+        return res.json(500,{
+            message:'Internal Server Error'
+        });
+
+    }
+}
