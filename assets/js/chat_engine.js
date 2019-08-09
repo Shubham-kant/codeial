5faa867aeae5b4e3d637b4722b33eadbe95d3584
Shubham-Kant <pro.shubham1999@gmail.com>
2019-08-10 01:32:04 +0530

    broadcasting message 'user is typing' to other users

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -32,19 +32,41 @@ class chatEngine{
         
         $('#send-message').click(function(){
             let msg = $('#chat-message-input').val();
+            // $("#user-chat-box").stop().animate({ scrollTop: $("#user-chat-box")[0].scrollHeight}, 1000);
 
             if (msg != ''){
+                $('#chat-message-input').val('');
                 self.socket.emit('send_message', {
                     message: msg,
                     user_email: self.userEmail,
                     chatroom: 'codeial'
                 });
+               
+                
             }
         });
+        //sending request for broadcasting if a user presses any key during chat
+        $('#chat-message-input').on('keypress',function(){
+            self.socket.emit('typing',{
+                user_email:self.userEmail
+            });
+
+        });
+        //receiving request of broadcasting msg to other users 
+        self.socket.on('typing',function(data){
+            console.log('data is',data.user_email);
+            $('#feedback').html(data.user_email+'<i>is typing a message<i>')
+        });
+        //receiving the receieve_message request
         self.socket.on('receive_message',function(data){
             console.log('message recieved ',data.message);
+
+            $('#feedback').html('');
+            
+
             let newMessage=$('<li>');
             let messageType='other-message';
+
             if(self.userEmail==data.user_email){
                 messageType='self-message';
             }
@@ -56,6 +78,7 @@ class chatEngine{
             }));
             newMessage.addClass(messageType);
             $('#chat-messages-list').append(newMessage);
+            
 
         })
         
