9fd9338e743b304f4a8b88ce3131cca6ca0dddba
Shubham-Kant <pro.shubham1999@gmail.com>
2020-04-25 20:24:10 +0530

    changes in mongoose file

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 const env=require('./environment');
-mongoose.connect(`mongodb://localhost/${env.db}`);
+mongoose.connect(process.env.MONGODB_URI||`mongodb://localhost/${env.db}`);
 
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error in connecting to Mongodb'));
