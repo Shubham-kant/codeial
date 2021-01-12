b4d66b1fbdeefb728499f6bae436199055e50e5f
Shubham-Kant <pro.shubham1999@gmail.com>
2021-01-12 18:01:28 +0530

    updated npm  packages

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ const rfs=require('rotating-file-stream');
 const path=require('path');
 const logDirectory=path.join(__dirname,'../production_logs');
 fs.existsSync(logDirectory)||fs.mkdirSync(logDirectory);
-const accessLogStream=rfs('access.log',{
+const accessLogStream=rfs.createStream('access.log',{
     interval:'1d',
     path:logDirectory
 
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,14 @@
 const mongoose=require('mongoose');
 const env=require('./environment');
-mongoose.connect(process.env.MONGODB_URI ||`mongodb://localhost/${env.db}`);
-
+// mongoose.connect(process.env.MONGODB_URI ||`mongodb://localhost/${env.db}`);
+mongoose
+    .connect(process.env.MONGO_URL || `mongodb://localhost/${env.db}`,{
+        useUnifiedTopology:true,
+        useNewUrlParser:true,
+        useCreateIndex:true,
+    })
+    .then(()=> console.log('MongoDB connected....'))
+    .catch((err) => console.log(err));
 const db=mongoose.connection;
 db.on('error',console.error.bind(console,'error in connecting to Mongodb'));
 
